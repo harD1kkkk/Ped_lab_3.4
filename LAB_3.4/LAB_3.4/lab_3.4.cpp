548b71ec8b0dfecc46351decf619fc5fde09e5f8
harD1kkkk <137618635+harD1kkkk@users.noreply.github.com>
2024-10-05 16:02:00 +0300

    Add files via upload

---
+++ LAB_3.4/LAB_3.4/lab_3.4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+	double x, y, R;
+
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+
+	bool A = (x >= 0 && y >= 0 && pow(x, 2) + pow(y, 2) <= pow(R, 2));
+	bool B = (x >= 0 && y <= 0 && pow(x, 2) + pow(y, 2) <= pow(R, 2));
+
+	if (A || B) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+	return 0;
+}
